3ce0660c749ea44bde13dcd01ac1e866dbca1908
hairuljr <januarhairul@gmail.com>
2020-09-10 08:47:42 +0700

    Refactor : Minimize codebase API
    Menghapus coding yang tidak diperlukan untuk API

--- resources/views/includes/backend/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="main-footer">
-  <div class="footer-left">
-    Copyright &copy; {{ date('Y') }} <div class="bullet"></div>Loker Programmer
-  </div>
-</footer>
--- resources/views/pages/front/kelas/success-checkout.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="main-content">
-  {{ 'Selamat proses pembayaran kelas premium anda sudah berhasil!' }}
-  <a href="{{ route('home') }}" class="btn btn-primary">Kembali</a>
-</div>
-@endsection
