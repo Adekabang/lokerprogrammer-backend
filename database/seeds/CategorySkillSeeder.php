161f960641df1ed7ccec591f3ae75951c348fc2f
muhamad wildan <wildanmuhamad961@gmail.com>
2020-09-29 13:27:05 +0700

    + data seeder skills

---
+++ database/seeds/CategorySkillSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySkillSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('category_skills')->insert([
+            [
+                'id' => 1,
+                'category_name' => 'Android',
+                'slug' =>'android',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'id' => 2,
+                'category_name' => 'Web Developer',
+                'slug' =>'Web-Developer',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'id' => 3,
+                'category_name' => 'Full Stack',
+                'slug' =>'full-stack',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            
+        ]);
+    }  
+}
---
+++ database/seeds/SkillSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SkillSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('member_skills')->insert([
+            [
+                'id' => 1,
+                'category_skills_id' =>1,
+                'skill_name' =>'React Native',
+                'skills_persentase'=>'98%',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'id' => 2,
+                'category_skills_id' =>2,
+                'skill_name' =>'Laravel, vue js',
+                'skills_persentase'=>'75%',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'id' => 3,
+                'category_skills_id' =>3,
+                'skill_name' =>'Laravel, vue js, React js, Restfull API',
+                'skills_persentase'=>'70%',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            
+        ]);  
+    }
+}
